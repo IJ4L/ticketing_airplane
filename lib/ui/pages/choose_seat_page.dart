6d296947acea9ba9305d2f1d2cd31c3e89735693
ijal <rijalfarhan19@gmail.com>
2023-05-06 23:04:17 +0800

    update ui

--- lib/ui/pages/choose_seat_page.dart
+++ lib/ui/pages/choose_seat_page.dart
@@ -18,12 +18,56 @@ class ChooseSeatPage extends StatelessWidget {
       );
     }
 
+    Widget seatStatus() {
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        child: Row(
+          children: [
+            Container(
+              width: 16,
+              height: 16,
+              margin: const EdgeInsets.only(left: 10, right: 6),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/icons/seat_avaible.png'),
+                ),
+              ),
+            ),
+            Text('Available', style: blackTextStyle),
+            Container(
+              width: 16,
+              height: 16,
+              margin: const EdgeInsets.only(left: 10, right: 6),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/icons/seat_selected.png'),
+                ),
+              ),
+            ),
+            Text('Selected', style: blackTextStyle),
+            Container(
+              width: 16,
+              height: 16,
+              margin: const EdgeInsets.only(left: 10, right: 6),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/icons/seat_unavabile.png'),
+                ),
+              ),
+            ),
+            Text('Unavailable', style: blackTextStyle),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 24),
         children: [
           title(),
+          seatStatus(),
         ],
       ),
     );
